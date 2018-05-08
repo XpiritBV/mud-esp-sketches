c812835115b423f0030abf345bb43332d35a5393
Loek <admin@admin>
2018-05-08 20:47:54 +0200

    add light

--- Basic/Light/Light.ino
+++ Basic/Light/Light.ino
@@ -1,15 +1,14 @@
-#include <BH1750.h>
+#include "BH1750.h"
 #include <Wire.h>
 
 /* Connect as follows:
  *  VCC --> 3v3
  *  GND --> GND
- *  SDA --> D1
- *  SCL --> D2
+ *  SDA --> D2
+ *  SCL --> D3
  *  ADD --> Not connected
  *  
- *  You need the BH1750 library from here: https://github.com/claws/BH1750
- *  Download repo as ZIP file. Then install in Arduino using Sketch --> Include Library --> Add .ZIP library
+  Start the Serial Monitor in the Tools menu.
  */
 
 BH1750 lightMeter;
@@ -19,12 +18,13 @@ void setup(){
   Serial.begin(115200);
 
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
-  // SCL = D2, SDA = D1
-  Wire.begin(D1, D2);
+  // SCL = D3, SDA = D2
+  Wire.begin(D2, D3);
 
   lightMeter.begin();
   Serial.println(F("BH1750 Test"));
 
+  pinMode(D1, OUTPUT);    
 }
 
 void loop() {
@@ -33,5 +33,6 @@ void loop() {
   Serial.print(lux);
   Serial.println(" lx");
   delay(1000);
-
+  //turn on the LED
+  analogWrite(D1, 1024);
 }
