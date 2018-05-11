df0d242203ecfd1b0181f07385986e63bcb594dc
Kees Verhaar <kverhaar@gmail.com>
2018-05-11 10:20:00 +0200

    Modified LED demo to include both digital and analog writes.

--- Basic/LED/led/led.ino
+++ Basic/LED/led/led.ino
@@ -1,22 +1,60 @@
 #define ledPin1 D1
-#define ledPin2 D2
 
 void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
   pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
-  set(ledPin1);
-  set(ledPin2);  
-  delay(1000);
+ blinkFiveTimes();
+ fadeTwoTimes(); 
 }
 
-void set(int ledPin){
+void blinkFiveTimes()
+{
+  for(int i = 0; i < 5; i++) {
+    digitalSet(ledPin1, HIGH);
+    delay(200);
+    digitalSet(ledPin1, LOW);
+    delay(200);
+  }
+}
+
+void fadeTwoTimes() {
+  for(int i = 0; i< 2; i++)
+  {
+    fadeUpAndDown(ledPin1);
+  }
+}
+
+// Uses analogWrite() to write a PWM signal to the output pin. Effectively, this means the LED will be dimmed.
+// For an explanation on PWM: https://en.wikipedia.org/wiki/Pulse-width_modulation
+void fadeUpAndDown(int ledPin) {
+  for(int i = 0; i <= 1023; i++) {
+    Serial.print("Setting led pin: ");
+    Serial.print(ledPin);
+    Serial.print(" to value: ");
+    Serial.println(i);
+    analogWrite(ledPin, i);
+    delay(1); //Pause 1 millisecond, for effect.
+  }
+  for(int i = 1023; i > 0 ; i--) {
+    Serial.print("Setting led pin: ");
+    Serial.print(ledPin);
+    Serial.print(" to value: ");
+    Serial.println(i);
+    analogWrite(ledPin, i);
+    delay(1); //Pause 1 millisecond, for effect.
+  }
+}
+
+// Uses digitalWrite() to set output pin to HIGH or LOW value. This means the Led will be on or off.
+void digitalSet(int ledPin, int value){
   Serial.print("Setting led pin: ");
-  Serial.println(ledPin);
-   analogWrite(ledPin, random(1,1024));
+  Serial.print(ledPin);
+  Serial.print(" to value: ");
+  Serial.println(value);
+  digitalWrite(ledPin, value);
 }
 
